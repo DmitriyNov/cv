cdbdd2035b656abb1f3d8f98af71eb244f02c322
DmitriyNov <NovokrDm@yandex.ru>
2025-09-04 17:24:30 +0300

    Addet more information about creer in dropdowns

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
 import './styles/main.scss'
 
 document.body.classList.add('loaded');
+
+const dropdowns = document.querySelectorAll(".info__dropdown--container");
+
+dropdowns.forEach((dropdown) => {
+    const button = dropdown.querySelector(".info__dropdown--button");
+    const content = dropdown.querySelector(".info__dropdown");
+
+    button.addEventListener("click", () => {
+        button.classList.toggle("info__dropdown--buttonActive");
+        content.classList.toggle("info__dropdown--active");
+
+        if (content.classList.contains("info__dropdown--active")) {
+            content.style.height = content.scrollHeight + "px";
+        } else {
+            content.style.height = 0;
+        }
+    });
+});
